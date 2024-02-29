ed900ea3577e2037f2ac1fd45a2515ef298f8652
ElTejoman <aespinoza.ascue@gmail.com>
2024-02-28 22:32:38 -0500

    create a streamlit enviroment and modify Readme.md

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # SPRINT5
 construir y desplegar un panel de control de una aplicación web en un servicio en la nube.
+
+## Dashboard de Anuncios de Venta de Coches
+
+Este proyecto es un panel de control de una aplicación web que permite visualizar y explorar datos de anuncios de venta de coches. La aplicación utiliza Streamlit para crear una interfaz de usuario interactiva que muestra un histograma y un gráfico de dispersión basados en los datos del archivo CSV proporcionado.
+
+### Funcionalidad
+
+- La aplicación incluye opciones para construir un histograma y un gráfico de dispersión.
+- Utiliza casillas de verificación para permitir al usuario seleccionar qué gráfico desea ver.
+- Los gráficos se generan utilizando la biblioteca Plotly Express.
---
+++ app.py
@@ -0,0 +1,35 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+# Leer el archivo CSV en un DataFrame
+car_data = pd.read_csv('C:/Users/User/Desktop/Learning_Python/Learning_Python/Sprint 5/SPRINT5/vehicles_us.csv')
+
+# Encabezado de la aplicación
+st.header('Panel de Control de Anuncios de Venta de Coches')
+
+# Casilla de verificación para construir un histograma
+build_histogram = st.checkbox('Construir un histograma')
+
+if build_histogram: # Si la casilla de verificación está seleccionada
+    # Mensaje
+    st.write('Construyendo un histograma para el conjunto de datos de anuncios de venta de coches')
+    
+    # Construir el histograma
+    fig = px.histogram(car_data, x="odometer")
+    
+    # Mostrar el histograma
+    st.plotly_chart(fig, use_container_width=True)
+
+# Casilla de verificación para construir un gráfico de dispersión
+build_scatter = st.checkbox('Construir un gráfico de dispersión')
+
+if build_scatter: # Si la casilla de verificación está seleccionada
+    # Mensaje
+    st.write('Construyendo un gráfico de dispersión para el conjunto de datos de anuncios de venta de coches')
+    
+    # Construir el gráfico de dispersión
+    fig = px.scatter(car_data, x="odometer", y="price")
+    
+    # Mostrar el gráfico de dispersión
+    st.plotly_chart(fig, use_container_width=True)
